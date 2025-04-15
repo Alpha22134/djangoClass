0d224238ee82b29bfbeac62fb729c8684ca413a8
Alpha22134 <jehan7576@gmail.com>
2025-04-15 17:11:24 +0900

    업데이트

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
+
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/index.html'
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # Create your views here.
 def index(request):
