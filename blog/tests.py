e95c2c5453800d777003ffba995a161252ed8192
Alpha22134 <jehan7576@gmail.com>
2025-05-27 17:20:32 +0900

    Django 업데이트2025.05.27.02

--- blog/tests.py
+++ blog/tests.py
@@ -1,11 +1,16 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 
 # Create your tests here.
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump', 
+            password='somepassword')
+        self.user_obama = User.objects.create_user(username='obama', 
+            password='somepassword')
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -38,19 +43,27 @@ class TestView(TestCase):
 
         post_001 = Post.objects.create(
             title='첫 번째 포스트', 
-            content='Hello World. We are the wold.',)
+            content='Hello World. We are the wold.',
+            author=self.user_trump)
         post_002 = Post.objects.create(
             title='두 번째 포스트', 
-            content='1등이 전부는 아님',)
+            content='1등이 전부는 아님',
+            author=self.user_obama)
+        
         self.assertEqual(Post.objects.count(), 2)
 
         response = self.client.get('/blog/')
         soup = BeautifulSoup(response.content, 'html.parser')
+
         self.assertEqual(response.status_code, 200)
         main_area =soup.find('div', id='main-area')
+
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아직 게시물 없음.', main_area.text)
+
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
 
     def test_post_detail(self):
         post_001 = Post.objects.create(
